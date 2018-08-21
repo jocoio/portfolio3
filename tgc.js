a60d607365bc931bf7b7259ddb1103442fe70365
Jon Corbett <corbett.jo@husky.neu.edu>
2018-08-21 15:13:50 -0400

    image optimization

--- tgc.js
+++ tgc.js
@@ -6,3 +6,11 @@ $(function () {
   knows.scrollLeft = $(knows).children().eq(0).width() + 20 - (($(window).width() / 2) - ($(knows).children().eq(1).width() / 2));
   stand.scrollLeft = $(stand).children().eq(0).width() + 20 - (($(stand).width() / 2) - ($(stand).children().eq(1).width() / 2));
 });
+
+$(function () {
+  var knows = document.getElementById("knows");
+  var stand = document.getElementById("stand");
+
+  knows.scrollLeft = $(knows).children().eq(0).width() + 20 - (($(window).width() / 2) - ($(knows).children().eq(1).width() / 2));
+  stand.scrollLeft = $(stand).children().eq(0).width() + 20 - (($(stand).width() / 2) - ($(stand).children().eq(1).width() / 2));
+});
